458fe21770eb426afd51d95f85e9837d8bbcec6a
Sarthak Agarwal <95269183+CARLO-IITI@users.noreply.github.com>
2022-12-31 15:30:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import {data} from "./data";
+import Table from 'react-bootstrap/Table';import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [show, setshow] = useState(false);
+  const [name, setname] = useState("");
+  const [names, setnames] = useState([]);
+
+  const trigger = () => {
+    setshow(true)
+    const b = data.filter((item) => item.name.toLowerCase().includes(name.toLowerCase()))
+    console.log(b)
+    setnames(b);
+  };
+
+  const TableDiv = () => {
+    return (
+      <Table striped bordered hover style={{margin: 60, width: "95vw"}}>
+        <thead><td>S. no.</td><td>Name</td><td>Telephone Number</td><td>Address</td></thead>
+        <tbody>{names.map((name,idx) => {
+          return (
+            <tr>
+            <td>{idx}</td>
+              <td>{name.name}</td>
+              <td>{name.phone}</td>
+              <td>{name.address}</td>
+            </tr>
+          );
+        })}</tbody>
+      </Table>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+        <button>Telephone Number Search</button>
+        <button>Company Search</button>
+        <button onClick={()=>{setshow(false)}}>Name Search</button>
+        <button>Change Number Search</button>
+      </div>
+      {!show ? (
+        <div className="main">
+          <h1>Search by Name</h1>
+          <br />
+          <div className="a">
+            <p>Name</p>
+            <input type="text" onChange={(e) => {setname(e.target.value)}} />
+          </div>
+          <button type="submit" onClick={trigger}>
+            Search
+          </button>
+        </div>
+      ) : (
+        <div className="main">
+        <TableDiv /></div>
+      )}
     </div>
   );
 }
